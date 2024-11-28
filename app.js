8e045385c554603009d4894f1db3de692c4b71b1
cristianCAOM <21610060@utgz.edu.mx>
2024-11-28 00:31:03 -0600

    29  commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('mi-repo/sw.js').then(registration => {
+        navigator.serviceWorker.register('sw.js').then(registration => {
             console.log('ServiceWorker registrado con éxito:', registration);
         }).catch(error => {
             console.log('Error en el registro del ServiceWorker:', error);
